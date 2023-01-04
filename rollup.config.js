29d4a22dfb7ef9d71bfda85b32f1f8125265980c
t.n.t <2638813712@qq.com>
2023-01-05 03:52:02 +0800

    Change the directory structure.

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel'
 import {uglify} from 'rollup-plugin-uglify';
 import { terser } from "rollup-plugin-terser";
 export default {
-    input: 'src/index.js',
+    input: 'index.js',
     output: {
       file: './dist/index.js',
       format: 'cjs',
